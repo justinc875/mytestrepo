2f68734673d12912a3e93fd490ec0b2cfd00ed75
BT_1N3_11 <sample@bths.edu>
2025-01-10 10:46:48 -0500

    updated print statement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello world");
-        System.out.println("justin chen");
+        System.out.println("justin chen hello");
         System.out.println("alex wang");
     }
 }
